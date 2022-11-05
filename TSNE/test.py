79781c08b00b112e03e12afff60ad19365e64272
Victoria Oberlander <victoria.oberlander@aalto.fi>
2022-11-05 17:34:29 +0200

    Adding TSNE

--- TSNE/test.py
+++ TSNE/test.py
@@ -1,11 +1,9 @@
-import pickle
 from os.path import dirname
 import numpy as np
-import scipy
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.manifold import TSNE
 
 # read in data
 file_path = "/home/oberlav1/scratch/graphics/oberlav1/junction22/global_outputs.npy"
@@ -19,11 +17,10 @@ for i, song in enumerate(range(num_songs)):
     logits = np.mean(songs[i]["taggram"], axis=0)
     feature_array[i,:] = logits/np.sum(logits)
 
-# run PCA
+# TSNE
 input_data = StandardScaler().fit_transform(feature_array)
-pca = PCA(n_components=num_features)
-principal_components = pca.fit_transform(input_data)
+tsne = TSNE(n_components=3, learning_rate=200, perplexity=100, verbose=2, angle=0.1)
+embedding = tsne.fit_transform(input_data)
 
-np.save(dirname(file_path) + "/principal_feature_components", principal_components)
-np.save(dirname(file_path) + "/explained_variance", pca.explained_variance_)
-np.save(dirname(file_path) + "/singular_values", pca.singular_values_)
+np.save(dirname(file_path) + "/tsne_embedding", embedding)
+
